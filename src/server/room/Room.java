c9a7bde90fabc7af70ed38711727d25063486669
Ivan Lysikov <livanalex@gmail.com>
2022-01-10 16:19:10 +0300

    Room constructor issue fixed

--- src/server/room/Room.java
+++ src/server/room/Room.java
@@ -9,7 +9,7 @@ public class Room {
         seats = new Seat[places.length][];
         for (int i = 0; i < places.length; i++) {
             seats[i] = new Seat[places[i]];
-            for (int j = 0; j < i; j++) {
+            for (int j = 0; j < places[i]; j++) {
                 seats[i][j] = new Seat(i, j);
             }
         }
