2a015746ced719f7f3af1a52d720f39090fd7624
AlionaSidarenka <a.sidarenka@eis-partners.com>
2022-01-16 01:40:52 +0300

    delete

--- src/server/crud/CRUD.java
+++ src/server/crud/CRUD.java
@@ -8,5 +8,5 @@ public interface CRUD <T> {
     void create(T t) throws FileAlreadyExistsException, JAXBException;
     T read(String name) throws FileNotFoundException, JAXBException;
     void update(T t) throws FileNotFoundException, JAXBException;
-    void delete(String name) throws FileNotFoundException;
+    void delete(T t) throws FileNotFoundException;
 }
--- src/server/crud/movie/CRUDMovie.java
+++ src/server/crud/movie/CRUDMovie.java
@@ -62,8 +62,8 @@ public class CRUDMovie implements CRUD<Movie> {
     }
 
     @Override
-    public void delete(String name) throws MovieNotFoundException {
-        File file = getFileOrThrowNotFound(name);
+    public void delete(Movie movie) throws MovieNotFoundException {
+        File file = getFileOrThrowNotFound(movie.getTitle());
         file.delete();
     }
 
